d20e9c5399284af6dc5959cd0d2b5be881466f8d
Eng-Ahmad-Almohammad <almohammedahmed2@gmail.com>
2021-10-30 19:28:45 +0200

    fix filter issue

--- courses/filters.py
+++ courses/filters.py
@@ -2,7 +2,7 @@ import django_filters as filters
 from .models import Rewads
 
 class RewardsFilter(filters.FilterSet):
-    id = filters.NumberFilter(field_name='owner__pk', lookup_expr='iexact')
+    id = filters.AllValuesFilter(field_name='owner__pk', lookup_expr='iexact')
   
 
     class Meta():
